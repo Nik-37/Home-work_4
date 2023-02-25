0b05edac4971aa1cd7a4e2de62067b2599b9fa53
Evgeniy <gmail.com>
2023-02-25 22:49:39 +0300

    add task_27

---
+++ task_27/Program.cs
@@ -0,0 +1,15 @@
+﻿int a;
+string str_a;
+int count;
+int sum = 0;
+
+Console.WriteLine("Введи число А: ");
+str_a = Console.ReadLine();
+a = Convert.ToInt32(str_a);
+while (a > 0)
+{
+    count = a % 10;
+    sum = sum + count;
+    a = a / 10;
+}
+Console.WriteLine(sum);
