86fdd092c48666c9cf6a0082a0832f8173c9dfd9
Evgeniy <gmail.com>
2023-02-25 21:02:45 +0300

    add task_29

---
+++ task_29/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введи количество элементов массива:\t");
+int elements = int.Parse(Console.ReadLine());
+int[] array = new int[elements];
+Console.WriteLine("Вывод массива:");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
