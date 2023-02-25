b8f45df747a6037be40bae5d905dfabeac2619a9
Evgeniy <gmail.com>
2023-02-25 20:56:31 +0300

    add task_25

---
+++ task_25/Program.cs
@@ -0,0 +1,18 @@
+﻿int A;
+int B;
+int result = 1;
+string str;
+
+Console.WriteLine("Введи число А: ");
+str = Console.ReadLine();
+A = Convert.ToInt32(str);
+Console.WriteLine("Веди число В: ");
+str = Console.ReadLine();
+B = Convert.ToInt32(str);
+
+for (int i = 1; i <= B; i ++)
+{
+result = result * A;
+}
+Console.WriteLine("Значение числа возведенного в степень =  " + result);
+
